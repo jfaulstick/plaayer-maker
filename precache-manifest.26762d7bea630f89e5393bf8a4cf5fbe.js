d9bb9969e931712ebf96c8cd814a240f900d563d
Joe Faulstick <jfaulstick@gmail.com>
2020-07-15 14:17:39 -0400

    Updates

---
+++ precache-manifest.26762d7bea630f89e5393bf8a4cf5fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf27800c3a0508c9a9454829c3e17851",
+    "url": "/plaayer-maker/index.html"
+  },
+  {
+    "revision": "a21254eaf73204bd883f",
+    "url": "/plaayer-maker/static/css/main.19c5c440.chunk.css"
+  },
+  {
+    "revision": "ae0ba5f810abcc848b94",
+    "url": "/plaayer-maker/static/js/2.34bd9495.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/plaayer-maker/static/js/2.34bd9495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21254eaf73204bd883f",
+    "url": "/plaayer-maker/static/js/main.b1b9b4e2.chunk.js"
+  },
+  {
+    "revision": "f09dba7cfcd3d68f8e11",
+    "url": "/plaayer-maker/static/js/runtime-main.c8f335df.js"
+  }
+]);
